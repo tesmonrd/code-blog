6c8425977bd47a0e2d686f0fd69604bbd91d5ebf
Rick Tesmond <tesmonrd@gmail.com>
2015-11-30 13:45:06 -0800

    renamed index

--- article.js
+++ article.js
@@ -7,11 +7,14 @@ var Article = function(title, category, author, authorUrl, publishedOn, body){
   this.authorUrl = authorUrl;
   this.publishedOn = publishedOn;
   this.body = body;
-  collectedEntries.push(this)
+  collectedEntries.push(this);
 }
 
 Article.prototype.toHTML = function() {
   return "<article>" +
   "<h1>" + this.title + "</h1>" +
+  "<h5>" + this.publishedOn + "</h5>" +
+  "<li>" + this.author + " " + this.authorUrl+ "</li>" +
+  "<p>" + this.body + "</p>"
   "</article>"
-}
+};
