8507b92fe03be4f1208853bf6e1015b2f79a743f
Rick Tesmond <tesmonrd@gmail.com>
2015-12-01 19:31:58 -0800

    created prelim event call for sorting out via forms. def need help

--- events.js
+++ events.js
@@ -1,4 +1,11 @@
 // create filter event for when option is selected with form
+filterBySelection= function(){
+  $('option').on('click',function(){
+    event.preventDefault();
+    $('article :selected(option)').hide();
+  });
+};
+
 blog.truncateArticles= function() {
   $('article p:not(:first-child)').hide();
   $('.read-on').on('click',function() {
