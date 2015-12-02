64c8e8cca03da9c0805a8d733662fb493476d17e
Rick Tesmond <tesmonrd@gmail.com>
2015-12-01 19:10:09 -0800

    removed incomplete event

--- events.js
+++ events.js
@@ -1,8 +1,4 @@
 // create filter event for when option is selected with form
-// blog.filterByAuthor= function() {
-//   $('')
-//
-
 blog.truncateArticles= function() {
   $('article p:not(:first-child)').hide();
   $('.read-on').on('click',function() {
