c0518d652cb140984a8b7a937ab985d6a3241517
Rick Tesmond <tesmonrd@gmail.com>
2015-12-02 10:00:12 -0800

    updated time reader function

--- article.js
+++ article.js
@@ -9,29 +9,29 @@ var Article = function(blog) {
   this.authorUrl = blog.authorUrl;
   this.publishedOn = blog.publishedOn;
   this.body = blog.body;
-  // this.time = this.timeRead(this.publishedOn);
+  this.time = this.timeRead(this.publishedOn);
   collectedEntries.push(this);
 };
 
-// Article.prototype.timeRead = function(date) {
-//   var time = new Date();
-//   var day = parseInt(today.getDate());
-//   var month = parseInt(today.getMonth()+1);
-//   var year = parseInt(today.getFullYear());
-//
-//   var yr = parseInt(date.slice(0,4));
-//   var mnth = parseInt(date,slice(5,7));
-//   var dy = parseInt(date.slice(8,10));
-//
-//   var smallerSegments = 24*60*60*1000
-//   var publishDate = new Date(yr, mnth, dy);
-//   var dateToday = new Date(year, month, day);
-//
-//   var calcDay = Math.round(Math.abs((publishDate.getTime()- dateToday.getTime())/(smallerSegments)));
-//   return
-// }
+Article.prototype.timeRead = function(date) {
+  var today = new Date();
+  var day = parseInt(today.getDate());
+  var month = parseInt(today.getMonth()+1);
+  var year = parseInt(today.getFullYear());
+
+  var yr = parseInt(date.slice(0,4));
+  var mnth = parseInt(date.slice(5,7));
+  var dy = parseInt(date.slice(8,10));
+
+  var smallerSegments = 24*60*60*1000
+  var publishDate = new Date(yr, mnth, dy);
+  var dateToday = new Date(year, month, day);
+
+  var calcDay = Math.round(Math.abs((publishDate.getTime()- dateToday.getTime())/(smallerSegments)));
+  return
+}
 Article.prototype.toHTML = function() {
-  // var time = this.timeRead(this.publishedOn);
+  var time = this.timeRead(this.publishedOn);
   $template = $('#template').clone();
   $template.removeAttr('id');
   $template.find('.title').html(this.title);
