7e3560f0b03e9f45cb779826f875efd7da14089e
Rick Tesmond <tesmonrd@gmail.com>
2015-12-02 16:08:08 -0800

    fully working filters

--- events.js
+++ events.js
@@ -1,31 +1,28 @@
-// create filter event for when option is selected with form
-// filterBySelection= function(){
-//   $('option').on('click',function(){
-//     event.preventDefault();
-//     $('article category:selected(.authorList)').hide();
-//   });
-// };
-var sortByAuthor = function() {
-  $('select[id="select-Author"]').change(function(){
-    var selection = $('select option:selected').val();
-    $('main').find('article:contains(' + selection + ')');
-    if($(this).val() !== 'none'){
-      $('.category:not(:contains(' + $(this).val() + '))').parent().hide();
-    }
-  });
-};
-
-var sortByCategory = function() {
-  $('select[id="category"]').change(function(){
-    $('#filterCategory').find('option:first').attr('selected','selected');
+var sortByAuthor = function(event) {
+  event.preventDefault();
+  $('main').find('article').hide();
+  if (event.target.value !== 'none') {
+    $('main').find('article').filter(function() {
+      return $(this).data('author') === event.target.value;
+    }).show();
+  } else {
     $('main').find('article').show();
-    if($(this).val() !== 'none'){
-      $('.category:not(:contains(' + $(this).val() + '))').parent().hide();
-    }
-  });
+  }
 };
 
-blog.truncateArticles= function() {
+var sortByCategory = function(event) {
+  event.preventDefault();
+  $('main').find('article').hide();
+  if (event.target.value !== 'none') {
+    $('main').find('article').filter(function() {
+      return $(this).data('category') === event.target.value;
+    }).show();
+  } else {
+    $('main').find('article').show();
+  }
+};
+
+blog.truncateArticles = function() {
   $('article p:not(:first-child)').hide();
   $('.read-on').on('click', function() {
     event.preventDefault();
