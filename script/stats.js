a5edf196a9bb2e9c2145851fa1f93079ca25faf7
Rick Tesmond <tesmonrd@gmail.com>
2015-12-08 16:10:17 -0800

    displaying author total working

--- script/stats.js
+++ script/stats.js
@@ -4,13 +4,34 @@
   // <figure out a way to map article and reduce> get into p tags an
 // average word length by author
 
-$.getJSON('script/hackerIpsum.json', function(blog) {
-  var blogArray = blog;
-  console.log(blogArray);
-  articleTotal = blog.length;
+// function pluck(property, collection) {
+//   console.log(collection);
+//   return collection.map(function(e) {
+//     return e[property];
+//   });
+// };
+
+var stats = {};
+
+stats.displayArtTotal = function() {
+  var articleTotal = stats.blogArray.length;
   $('#stats').append('<p>Total Number of Articles: ' + articleTotal + '</p>');
+};
 
+stats.displayAuthorTotal = function() {
+  var authorLog = [];
+  stats.blogArray.forEach(function(article){
+    if(authorLog.indexOf(article.author) === -1){
+      authorLog.push(article.author);
+    };
+  });
+  var authorTotal = authorLog.length;
+  $('#stats').append('<p>Total Number of Authors: ' + authorTotal + '</p>')
+};
 
-
-
-});
+$.getJSON('script/hackerIpsum.json', function(blog) {
+  stats.blogArray = blog;
+}).done(
+  stats.displayArtTotal,
+  stats.displayAuthorTotal
+);
