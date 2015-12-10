157e41bbd2babc8f1fb679c4da434f873e0f4ded
Rick Tesmond <tesmonrd@gmail.com>
2015-12-09 16:14:01 -0800

    added proto for sql actions

--- script/new.js
+++ script/new.js
@@ -27,9 +27,35 @@ ArticlePreview.prototype.publish = function() {
   $('#sampleArticle').append($template);
 };
 
+ArticlePreview.prototype.insertRecord = function(callback) {
+  webDB.execute(
+    [
+      {
+        'sql': 'INSERT INTO article (title, category, author, authorUrl, publishedOn, markdown) VALUE (?, ?, ?, ?, ?, ?);',
+        'data': [this.title, this.category,this.author, this.authorUrl, this.publishedOn, this.body],
+      }
+    ],
+    callback
+  );
+};
+
+ArticlePreview.prototype.updateRecord = function(callback) {
+  //update article record in databse
+  webDB.execute(
+    [
+      {
+        'sql': 'UPDATE article SET title = ?, category = ?, author = ?, authorUrl = ?, publishedOn = ?, markdown = ?, WHERE id= ?',
+        'data': [this.title, this.category,this.author, this.authorUrl, this.publishedOn, this.markdown, article.id],
+      }
+    ],
+    callback
+  );
+};
+
 var buildArticle = function () {
   var blogPost = new ArticlePreview (articleFromPage);
   blogPost.publish();
+
 };
 
 $('#Article').on('submit', function(event){
