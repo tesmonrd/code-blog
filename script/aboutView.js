4d3b6afe238d8e2da7184fd7f143044a0ead1190
Rick Tesmond <tesmonrd@gmail.com>
2015-12-15 14:20:36 -0800

    changed about render function and cleaned data for successful rendering

--- script/aboutView.js
+++ script/aboutView.js
@@ -2,26 +2,18 @@ var aboutView = {};
 
 aboutView.index = function() {
   aboutView.ui();
-
-  var _append = function(repos) {
-    $('#aboutContent ul').append(aboutView.render(repos));
-  };
-
-  repos.all.filter(function(repos) {
-    return stargazers_count; //filter by any property in the repo object
-  }).map(_append)
+  aboutView.render();
 };
 
 aboutView.render = function () {
-  $('#articles').hide();
-  $('#aboutContent').fadeIn();
-  return $('<li>').text(repo.full_name);
-}
+  repos.all.forEach(function(repo){
+    $('#aboutContent ul').append('<li>' + repo.full_name + '</li>');
+  });
+};
 
 aboutView.ui = function() {
   var $about = $('#aboutContent');
   var $ul = $about.find('ul');
-
-  $ul.empty();
-  $about.fadeIn().siblings().hide();
+  $('#articles').hide();
+  $about.fadeIn();
 };
