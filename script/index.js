e135d36133f598a710b76033e0de2d88bf73c80b
Rick Tesmond <tesmonrd@gmail.com>
2015-12-17 15:39:08 -0800

    removed old functions

--- script/index.js
+++ script/index.js
@@ -1,7 +1,6 @@
 // ----------- Function Calls ---------------//
 $(document).ready(function(){
   webDB.init();
-  articlesController.index();
   $('#filterAuthor').change(sortByAuthor);
   $('#filterCategory').change(sortByCategory);
   $('#aboutContent').hide();
